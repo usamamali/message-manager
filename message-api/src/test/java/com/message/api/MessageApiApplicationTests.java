261deb30a18d9dd4da342d8f79c6a37f37a4b70f
Usama M. Ali <eng.usama.muhammad@gmail.com>
2019-10-23 16:10:46 +0200

    add dockerfile for dev and test cases

--- message-api/src/test/java/com/message/api/MessageApiApplicationTests.java
+++ message-api/src/test/java/com/message/api/MessageApiApplicationTests.java
@@ -1,13 +1,20 @@
 package com.message.api;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.Assert.assertTrue;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
 class MessageApiApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(Boolean.TRUE);
 	}
 
 }
